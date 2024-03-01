bca1f105b1711df698d4edbc68ca579c2ed4084f
Edward Ferdian <edwardferdian03@gmail.com>
2024-03-02 12:09:34 +1300

    2D trainer  script

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # diffusion_tutorial
+
+Create a new environment with Python 3.10 and install the package in editable mode:
+
+```bash
+conda create -n tutorial python=3.10
+conda activate tutorial
+pip install -e .
+```
+
+### 2D diffusion model
+
+#### Training
+
+1. Put the data in the `data` folder
+2. Run the training script
+
+```bash
+python trainer_2d.py --data-dir data --output-dir models
+```
+
+#### Inference
+
+1. Run the inference script
+
+```bash
+python sampler_2d.py --model-path models/model.pth --output-dir results
+```
---
+++ trainer_2d.py
@@ -0,0 +1,44 @@
+from denoising_diffusion_pytorch import Unet, GaussianDiffusion, Trainer
+import argparse
+
+if __name__ == "__main__":
+    # read data_dir and output_dir from command line
+    
+    parser = argparse.ArgumentParser(description='Train a diffusion model')
+    parser.add_argument('--data-dir', type=str, required=True, help='Directory containing the training images')
+    parser.add_argument('--output-dir', type=str, required=True, help='Directory to save the trained model')
+    parser.add_argument('--channels', type=int, default=3, help='Number of channels in the input image')
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    model_dir = args.output_dir
+    channel = args.channels
+
+    model = Unet(
+        dim = 64,
+        dim_mults = (1, 2, 4, 8),
+        channels = channel # 3 for color images, 1 for grayscale images
+    ).cuda()
+
+    diffusion = GaussianDiffusion(
+        model,
+        image_size = 64,
+        timesteps = 1000,           
+        sampling_timesteps = 100,   
+    ).cuda()
+
+    trainer = Trainer(
+        diffusion,
+        data_dir,
+        train_batch_size = 32,
+        train_lr = 8e-5,
+        train_num_steps = 50000,         # total training steps
+        gradient_accumulate_every = 2,    # gradient accumulation steps
+        ema_decay = 0.995,                # exponential moving average decay
+        results_folder = model_dir,
+        num_samples=16,
+        calculate_fid=False,
+        save_and_sample_every = 1000,
+    )
+
+    trainer.train()
